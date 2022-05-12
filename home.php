d12c9c8dd0cad6c7d044f3a1ac48d22dcdbe8f55
Sepehr Katebi <sepehrkatebi61@gmail.com>
2022-05-13 01:09:44 +0400

    Chat system (Closed Beta V0.2)
    Add color-theme meta tag to the client interface pages

--- home.php
+++ home.php
@@ -11,6 +11,8 @@
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <meta name="theme-color" media="(prefers-color-scheme: light)" content="#4e60ff">
+     <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000">
      <link rel="shortcut icon" href="./assets/favicon/favicon.ico" type="image/x-icon">
      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
      <link rel="stylesheet" href="./css/style.css">
